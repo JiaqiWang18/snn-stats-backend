688e241fbf6b447b255398e09a036d3bc00a46ac
Jacky Wang <jacky200218@gmail.com>
2020-09-20 22:12:24 -0700

    automate oc cities

--- server/server.py
+++ server/server.py
@@ -1,3 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from flask import Flask, request, jsonify,render_template
 from datetime import date, timedelta
 from bs4 import BeautifulSoup
@@ -135,6 +141,49 @@ def updateLog():
             con.commit()
     con.close()
 
+def crawlOCCities():
+    options = Options()
+    options.headless = True
+
+    options.add_argument("--window-size=1920,1200")
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver.get('https://ochca.maps.arcgis.com/apps/opsdashboard/index.html#/2a169f85c2254dd7b43f95b095208356')
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "ember273"))
+        )
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        div = soup.find_all("div", {"class": "external-html"})
+        raw = []
+        for each in div:
+            children = each.findChildren("p", recursive=False)
+            for child in children:
+                raw.append(child.text.split('-\xa0'))
+        processed = []
+        for each in raw:
+            if (len(each) == 2):
+                try:
+                    number = int(each[1].split(" ")[0].replace(",", ""))
+                    processed.append((each[0], number))
+                except Exception:
+                    continue
+        processed = processed[:40]
+        con = mysql.connector.connect(user='admin', password='Jiaqi200218',
+                                      host='collegedata.cwfud0qzqwsy.us-east-2.rds.amazonaws.com',
+                                      database='snn')
+        cursor = con.cursor()
+        sql = "update occities set stats = %s where cityname = %s"
+        for cityset in processed:
+            cursor.execute(sql, (cityset[1], cityset[0]))
+            con.commit()
+        con.close()
+        return (processed)
+
+        # div = soup.find_all("div", {"class": 'maincounter-number'})
+    finally:
+        driver.quit()
+
 @app.route('/')
 
 def index():
@@ -233,6 +282,10 @@ def getGraphData():
 
     con.close()
     return jsonify(output)
+
+@app.route('/crawlOCCities')
+def crawlOCCityData():
+    return jsonify(crawlOCCities())
 
 if __name__ == '__main__':
     #crawlOCCity("https://ochca.maps.arcgis.com/apps/opsdashboard/index.html#/cc4859c8c522496b9f21c451de2fedae")
