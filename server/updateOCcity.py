e5bf2f4f9f140f4935a38cb932d66e071e57bb93
Jacky Wang <jacky200218@gmail.com>
2020-11-27 11:33:25 -0800

    security

--- server/updateOCcity.py
+++ server/updateOCcity.py
@@ -16,9 +16,9 @@ def test_func():
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    user_agent = 'Mozilla/5.0'
     ua = UserAgent()
-    user_agent = ua.random
+    #user_agent = ua.random
     options.add_argument('user-agent={0}'.format(user_agent))
     options.add_argument("start-maximized")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
