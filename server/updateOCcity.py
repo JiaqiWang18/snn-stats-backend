4874fb0809aecec434595d6d510af04a0c8d4ec9
Jacky Wang <jacky200218@gmail.com>
2020-11-26 18:30:46 -0800

    security

--- server/updateOCcity.py
+++ server/updateOCcity.py
@@ -40,6 +40,42 @@ def send(message):
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(EMAIL_ADDRESS, EMAIL_PASS)
         server.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, message)
+def crawlOCCities():
+    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google-chrome'
+    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+    options = webdriver.ChromeOptions()
+    options.binary_location = GOOGLE_CHROME_PATH
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
 
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
+    driver.get("https://ochca.maps.arcgis.com/apps/opsdashboard/index.html#/2a169f85c2254dd7b43f95b095208356")
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "ember273"))
+        )
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        div = soup.find_all("div", {"class": "external-html"})
+        raw = []
+        for each in div:
+            children = each.findChildren("p", recursive=False)
+            for child in children:
+                raw.append(child.text.split('-\xa0'))
+        processed = []
+        for each in raw:
+            if (len(each) == 2):
+                try:
+                    number = int(each[1].split(" ")[0].replace(",", ""))
+                    processed.append((each[0], number))
+                except Exception:
+                    continue
+        processed = processed[:40]
+
+        return (processed)
+
+        # div = soup.find_all("div", {"class": 'maincounter-number'})
+    finally:
+        driver.quit()
 if __name__ == '__main__':
-  print(test_func())
+  print(crawlOCCities())
