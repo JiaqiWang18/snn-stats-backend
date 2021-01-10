12d87031e6a14e09e97b3cf983612f96c023eb42
Jacky Wang <jacky200218@gmail.com>
2021-01-10 11:17:40 -0800

    add and log school cases

--- server/server.py
+++ server/server.py
@@ -13,10 +13,10 @@ import os
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
-app.config['TEMPLATES_AUTO_RELOAD']=True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 CORS(app)
 
-#config vars
+# config vars
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google-chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 DATABASE_URL = os.environ.get("SNN_RDS_URL")
@@ -28,6 +28,7 @@ OC_SOURCE = os.environ.get("COVID_OC_SOURCE")
 OCCITIES_SOURCE = os.environ.get("OCCITIES_SOURCE")
 SCHOOL_SOURCE = os.environ.get("SCHOOL_SOURCE")
 
+
 def crawlUS(link):
     agent = {"User-Agent": "Mozilla/5.0"}
     page = requests.get(link, headers=agent)
@@ -37,67 +38,71 @@ def crawlUS(link):
     for each in div:
         children = each.findChildren("span", recursive=False)
         for child in children:
-            raw.append(int(child.text.replace(",","")))
+            raw.append(int(child.text.replace(",", "")))
 
-    if(len(raw)!=3):
+    if (len(raw) != 3):
         raise Exception("US crawling exception")
     return {
-        "US Total Cases （美国总共）":raw[0],
-        "US Deaths （美国死亡）":raw[1],
-        "US Recovered （美国康复）":raw[2]
+        "US Total Cases （美国总共）": raw[0],
+        "US Deaths （美国死亡）": raw[1],
+        "US Recovered （美国康复）": raw[2]
     }
+
 
 def crawlCa(link):
     agent = {"User-Agent": "Mozilla/5.0"}
     page = requests.get(link, headers=agent)
     soup = BeautifulSoup(page.content, 'html.parser')
     each = soup.find_all("div", {"class": 'big-number'})
-    each = [int(info.text.replace(",","")) for info in each]
+    each = [int(info.text.replace(",", "")) for info in each]
     print(each)
     if (len(each) != 3):
         raise Exception("CA crawling exception")
     return {
-        "CA Total （加州总共）":each[0],
-        "CA Deaths （加州死亡）":each[1]
+        "CA Total （加州总共）": each[0],
+        "CA Deaths （加州死亡）": each[1]
     }
+
 
 def crawlLa(link):
     agent = {"User-Agent": "Mozilla/5.0"}
     page = requests.get(link, headers=agent)
     soup = BeautifulSoup(page.content, 'html.parser')
     td = soup.find_all("td")
-    raw = [info.text.replace(",","") for info in td]
+    raw = [info.text.replace(",", "") for info in td]
     data = {}
 
-    for i in range(6,20):
-        print(str(i)+":"+str(raw[i]))
+    for i in range(6, 20):
+        print(str(i) + ":" + str(raw[i]))
 
-        if("Laboratory Confirmed Cases" in raw[i]):
-            data["LA Total （洛杉矶县总共）"] = int(raw[i+1])
-        elif("Deaths" in raw[i]):
+        if ("Laboratory Confirmed Cases" in raw[i]):
+            data["LA Total （洛杉矶县总共）"] = int(raw[i + 1])
+        elif ("Deaths" in raw[i]):
             data["LA Deaths （洛杉矶县死亡）"] = int(raw[i + 1])
             break
-    if(len(data)!=2):
-        raise Exception ("La crawling error")
+    if (len(data) != 2):
+        raise Exception("La crawling error")
     return data
+
 
 def crawlOC(link):
     agent = {"User-Agent": "Mozilla/5.0"}
     page = requests.get(link, headers=agent)
     soup = BeautifulSoup(page.content, 'html.parser')
     each = soup.find_all("h3", {"class": 'casecount-panel-title'})
-    each = [info.text.replace(",","").replace("*","") for info in each]
-    if(len(each)!=9):
+    each = [info.text.replace(",", "").replace("*", "") for info in each]
+    if (len(each) != 9):
         raise Exception("OC crawling error")
     return {
-        "OC Total （橙县总共）":int(each[0]),
-        "OC Increase （橙县新增）":int(each[1]),
-        "OC Death （橙县死亡）":int(each[2]),
-        "OC Death Increase （橙县新增死亡）":int(each[3]),
+        "OC Total （橙县总共）": int(each[0]),
+        "OC Increase （橙县新增）": int(each[1]),
+        "OC Death （橙县死亡）": int(each[2]),
+        "OC Death Increase （橙县新增死亡）": int(each[3]),
         "OC ICU（橙县ICU病例）": int(each[7]),
-        "OC Total Tested （橙县已检测）":int(each[4]),
-        "OC Hospitalized （橙县住院人数）":int(each[6]),
+        "OC Total Tested （橙县已检测）": int(each[4]),
+        "OC Hospitalized （橙县住院人数）": int(each[6]),
     }
+
 
 def crawlHS(link):
     agent = {"User-Agent": "Mozilla/5.0"}
@@ -112,6 +117,7 @@ def crawlHS(link):
 
     return output
 
+
 def updateYesterday():
     con = mysql.connector.connect(user='admin', password=DATABASE_PASS,
                                   host=DATABASE_URL,
@@ -121,9 +127,10 @@ def updateYesterday():
     current = cursor.fetchall()
     for dataset in current:
         sql = "UPDATE yesterdayStats SET Stats = %s WHERE Label = %s;"
-        cursor.execute(sql,(dataset[2],dataset[1]))
+        cursor.execute(sql, (dataset[2], dataset[1]))
         con.commit()
     con.close()
+
 
 def updateYesterOC():
     con = mysql.connector.connect(user='admin', password=DATABASE_PASS,
@@ -135,11 +142,12 @@ def updateYesterOC():
     current = cursor.fetchall()
     print(current)
     sql = "update yesteroc set stats = %s where cityname = %s"
-    #sql = "insert into yesteroc (cityname,stats) values (%s,%s)"
+    # sql = "insert into yesteroc (cityname,stats) values (%s,%s)"
     for cityset in current:
         cursor.execute(sql, (cityset[1], cityset[0]))
         con.commit()
     con.close()
+
 
 def updateLog():
     con = mysql.connector.connect(user='admin', password=DATABASE_PASS,
@@ -150,14 +158,14 @@ def updateLog():
     current = cursor.fetchall()
     dt = date.today() - timedelta(days=1)
     if dt.day < 10:
-        monthdate = float(dt.month + dt.day/100)
+        monthdate = float(dt.month + dt.day / 100)
 
     else:
         monthdate = float(str(dt.month) + "." + str(dt.day))
     cursor.execute(f"SELECT Date FROM logs WHERE Date = {monthdate}")
     checker = cursor.fetchall()
     print(checker)
-    if(len(checker)==0):
+    if (len(checker) == 0):
         cursor.execute(("INSERT INTO logs (Date) VALUES (%s)"), (monthdate,))
         con.commit()
         for sub in current:
@@ -165,6 +173,7 @@ def updateLog():
             cursor.execute(sql)
             con.commit()
     con.close()
+
 
 def updateSchool():
     con = mysql.connector.connect(user='admin', password=DATABASE_PASS,
@@ -188,15 +197,16 @@ def updateSchool():
         cursor.execute(("INSERT INTO schoolLog (Date) VALUES (%s)"), (monthdate,))
         con.commit()
         SQL = "UPDATE schoolLog SET "
-        ops = ["Population", "Student","Staff", "Rate"]
+        ops = ["Population", "Student", "Staff", "Rate"]
         for school_set in current:
             for op, data in zip(ops, school_set[1:]):
-                SQL += f"{school_set[0].replace(' ' , '')}{op} = {data}, "
-        SQL = SQL[:len(SQL)-2] + f"WHERE Date = {monthdate};"
+                SQL += f"{school_set[0].replace(' ', '')}{op} = {data}, "
+        SQL = SQL[:len(SQL) - 2] + f"WHERE Date = {monthdate};"
     print(SQL)
     cursor.execute(SQL)
     con.commit()
     con.close()
+
 
 def crawlOCCities():
     options = webdriver.ChromeOptions()
@@ -242,14 +252,16 @@ def crawlOCCities():
     finally:
         driver.quit()
 
-@app.route('/')
 
+@app.route('/')
 def index():
     return render_template("covidstats.html")
+
 
 @app.route('/snnfrontpage')
 def snnfront():
     return render_template("frontpage.html")
+
 
 @app.route('/crawlData')
 def crawlData():
@@ -266,12 +278,12 @@ def crawlData():
 
     counter = 1
     for sub in data:
-        for  key in sub:
+        for key in sub:
             sql = "UPDATE currentStats SET Stats = %s WHERE Label = %s"
-            val = (sub[key],key)
+            val = (sub[key], key)
             cursor.execute(sql, val)
             con.commit()
-            counter+=1
+            counter += 1
 
     school_data = crawlHS(SCHOOL_SOURCE)
     for school_name in school_data:
@@ -284,10 +296,11 @@ def crawlData():
     data.append(school_data)
     return jsonify(data)
 
+
 @app.route('/getData')
 def getData():
-    con = mysql.connector.connect(user='admin', password = DATABASE_PASS,
-                                  host= DATABASE_URL,
+    con = mysql.connector.connect(user='admin', password=DATABASE_PASS,
+                                  host=DATABASE_URL,
                                   database='snn')
     cursor = con.cursor()
     cursor.execute("SELECT * FROM currentStats")
@@ -295,12 +308,12 @@ def getData():
     cursor.execute("SELECT * FROM yesterdayStats")
     yesterday = cursor.fetchall()
 
-    if(len(current)-len(yesterday))!=0:
+    if (len(current) - len(yesterday)) != 0:
         raise Exception("unequal database")
-    output=dict()
+    output = dict()
     for i in range(len(current)):
-        output[current[i][1]] = [current[i][2], current[i][2]-yesterday[i][2]]
-    #output["OC Today Percentage of Positive Cases （橙县今日检测阳性概率）"] = [round(output['OC Total （橙县总共）'][1]/output['OC Total Tested （橙县已检测）'][1],3) ,0]
+        output[current[i][1]] = [current[i][2], current[i][2] - yesterday[i][2]]
+    # output["OC Today Percentage of Positive Cases （橙县今日检测阳性概率）"] = [round(output['OC Total （橙县总共）'][1]/output['OC Total Tested （橙县已检测）'][1],3) ,0]
 
     cursor.execute("SELECT * FROM occities ORDER BY stats DESC")
     occurrent = cursor.fetchall()
@@ -310,23 +323,23 @@ def getData():
     print(ocyesterday)
     dicocyesterday = {}
     for city in ocyesterday:
-        dicocyesterday[city[0]]=city[1]
+        dicocyesterday[city[0]] = city[1]
     print(dicocyesterday)
     for i in range(len(occurrent)):
-
-        output[occurrent[i][0]] = [occurrent[i][1], occurrent[i][1]-dicocyesterday[occurrent[i][0]]]
+        output[occurrent[i][0]] = [occurrent[i][1], occurrent[i][1] - dicocyesterday[occurrent[i][0]]]
     con.close()
     print(output)
     return jsonify(output)
 
-@app.route('/updateYesterday')
 
+@app.route('/updateYesterday')
 def setYesterday():
     updateYesterday()
     updateLog()
     updateYesterOC()
     updateSchool()
     return "working"
+
 
 @app.route("/graphData")
 def getGraphData():
@@ -342,7 +355,7 @@ def getGraphData():
     data = cursor.fetchall()
     cursor.execute("SELECT * FROM currentStats")
     current = cursor.fetchall()
-    MIN = len(data)-MIN
+    MIN = len(data) - MIN
     data = data[MIN:]
     yesterDate = str(data[-1][0]).split(".")
     dt = date(2021, int(yesterDate[0]), int(yesterDate[1])) + timedelta(days=1)
@@ -350,12 +363,12 @@ def getGraphData():
     monthdate = float(str(dt.month) + "." + addZero(str(dt.day)))
     output = {}
     for row in data:
-        output[float(row[0])]=[row[1],row[4],row[6],row[8]]
-    output[monthdate]=[current[0][2],current[3][2],current[5][2],current[7][2]]
-
+        output[float(row[0])] = [row[1], row[4], row[6], row[8]]
+    output[monthdate] = [current[0][2], current[3][2], current[5][2], current[7][2]]
 
     con.close()
     return jsonify(output)
+
 
 @app.route('/crawlOCCities')
 def crawlOCCityData():
